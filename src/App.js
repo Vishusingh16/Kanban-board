c0dbf25eeb701277b6ded38aa7c526755b3a86af
Vaishnav Singh <vaishnavsingh162@gmail.com>
2024-10-31 10:20:28 +0530

    new commit

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div className='font-semibold text-red-950'>App</div>
-  )
-}
-
-export default App
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Components/Header'
+import Center from './Components/Center'
+
+const App = () => {
+  return (
+   <div>
+    <Header />
+    <Center/>
+
+   </div>
+  )
+}
+
+export default App
